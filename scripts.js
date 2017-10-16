18ce157ff74fbf4db16a3fceb79a7d94f09d942f
Aaron <aa.griff9@gmail.com>
2017-10-16 13:10:17 -0700

    Font Styling & JS-smooth scrolling

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 $(function() {
 
+  //flickity carousel
+
   $('.main-carousel').flickity({
     cellAlign: 'left',
     contain: 'true',
@@ -9,6 +11,46 @@ $(function() {
     wrapAround: 'true',
   });
 
+  //Smooth Scrolling - (css-tricks.com)
+
+  $('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+  $('button').on('click', function() {
+    alert('Thanks for subscribing!');
+ });
 
 
 });
